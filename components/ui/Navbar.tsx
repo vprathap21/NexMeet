b5357f454434b8ae2da3efcdb1fa5c20c2a25aca
prathap v <211501064@rajalakshmi.edu.in>
2024-06-17 03:30:22 +0530

    Navbar.tsx updated

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import MobileNav from "./MobileNav";
+import { SignedIn, UserButton } from "@clerk/nextjs";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="flex-between fixed z-50 w-full bg-dar-1 px-6 py-4 lg:px-10">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          width={32}
+          height={32}
+          alt="yoom logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] text-white font-extrabold max-sm:hidden">
+          Yoom
+        </p>
+      </Link>
+      <div className="flex-beween gap-5">
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+         <MobileNav />
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
