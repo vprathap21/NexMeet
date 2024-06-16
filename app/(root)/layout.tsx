78f161cfb885d51deb8edbbdf876a8a8b266f8b1
prathap v <211501064@rajalakshmi.edu.in>
2024-06-17 01:11:40 +0530

    updated

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,11 @@
+import React, { Children, ReactNode } from 'react'
+
+const RootLayout = ({children}:{children:ReactNode}) => {
+  return (
+    <main>
+        {children}
+    </main>
+  )
+}
+
+export default RootLayout
