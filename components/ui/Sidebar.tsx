9dcbbb6f3d0fb57a468e979d03632c688bfc7372
prathap v <211501064@rajalakshmi.edu.in>
2024-06-17 03:31:19 +0530

    Sidebar.tsx updated

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -12,7 +12,7 @@ const Sidebar = () => {
       <div className="flex flex- flex-col gap-6 ">
         {sidebarLinks.map((link) => {
           const isActive =
-            pathname === link.route || pathname.startsWith(link.route);
+            pathname === link.route || pathname.startsWith(`${link.route}/`);
           return (
             <Link
               href={link.route}
